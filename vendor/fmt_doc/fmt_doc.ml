9c252bf24774dbd3a192a1f6f2088a371ee5c367
johnyob <ajo41@cam.ac.uk>
2023-03-14 20:34:51 +0000

    Implement `Fmt_doc`. Document-based formatter inspired by `Fmt`

---
+++ vendor/fmt_doc/fmt_doc.ml
@@ -0,0 +1,77 @@
+open Core
+
+type t = Format.formatter -> unit
+
+let empty _ppf = ()
+
+let repeat n t ppf =
+  for _ = 1 to n do
+    t ppf
+  done
+;;
+
+let newline ppf = Fmt.pf ppf "\n"
+let sp ppf = Fmt.sp ppf ()
+let cut ppf = Fmt.cut ppf ()
+let comma ppf = Fmt.comma ppf ()
+let semi ppf = Fmt.semi ppf ()
+
+let append t1 t2 ppf =
+  t1 ppf;
+  t2 ppf
+;;
+
+let ( ++ ) = append
+
+let concat ?(sep = cut) ts ppf =
+  let is_first = ref true in
+  List.iter ts ~f:(fun t ->
+    if !is_first then is_first := false else sep ppf;
+    t ppf)
+;;
+
+let box ?(indent = 0) t ppf =
+  Format.(
+    pp_open_box ppf indent;
+    t ppf;
+    pp_close_box ppf ())
+;;
+
+let hbox t ppf =
+  Format.(
+    pp_open_hbox ppf ();
+    t ppf;
+    pp_close_box ppf ())
+;;
+
+let vbox ?(indent = 0) t ppf =
+  Format.(
+    pp_open_vbox ppf indent;
+    t ppf;
+    pp_close_box ppf ())
+;;
+
+let hvbox ?(indent = 0) t ppf =
+  Format.(
+    pp_open_hvbox ppf indent;
+    t ppf;
+    pp_close_box ppf ())
+;;
+
+let hovbox ?(indent = 0) t ppf =
+  Format.(
+    pp_open_hovbox ppf indent;
+    t ppf;
+    pp_close_box ppf ())
+;;
+
+let char chr ppf = Fmt.char ppf chr
+let string str ppf = Fmt.string ppf str
+let int n ppf = Fmt.int ppf n
+let bool b ppf = Fmt.bool ppf b
+let styled style t ppf = Fmt.styled style (fun ppf () -> t ppf) ppf ()
+let of_pp pp val_ ppf = pp ppf val_
+
+let option ?(none = empty) some opt ppf =
+  Option.value ~default:none (Option.map opt ~f:some) ppf
+;;
---
+++ vendor/fmt_doc/fmt_doc.mli
@@ -0,0 +1,24 @@
+type t
+
+val empty : t
+val repeat : int -> t -> t
+val newline : t
+val sp : t
+val cut : t
+val comma : t
+val semi : t
+val append : t -> t -> t
+val ( ++ ) : t -> t -> t
+val concat : ?sep:t -> t list -> t
+val box : ?indent:int -> t -> t
+val hbox : t -> t
+val vbox : ?indent:int -> t -> t
+val hvbox : ?indent:int -> t -> t
+val hovbox : ?indent:int -> t -> t
+val char : char -> t
+val bool : bool -> t
+val int : int -> t
+val string : string -> t
+val styled : Fmt.style -> t -> t
+val of_pp : 'a Fmt.t -> 'a -> t
+val option : ?none:t -> ('a -> t) -> 'a option -> t
